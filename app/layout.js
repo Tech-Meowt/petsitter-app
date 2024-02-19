03a8d270f17a359f88263f99a53db21a7a10ef30
jessicagallagher <jessicalyngallagher@me.com>
2024-02-19 14:51:55 -0500

    add basic homepage with title and metadata

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,6 @@ import { PrismicPreview } from '@prismicio/next';
 import { repositoryName } from '@/prismicio';
 import './globals.css';
 
-export const metadata = {
-  title: "Jenni's Cat Care",
-  description: 'A demo petsitting web application from Tech Meowt',
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
--- app/page.js
+++ app/page.js
@@ -3,7 +3,28 @@ import * as prismic from '@prismicio/client';
 import { SliceZone } from '@prismicio/react';
 // import { components } from '@/slices';
 
-export default function Home() {
+const queryHomepage = () => {
+  const client = createClient();
+  return client.getSingle('homepage');
+};
+
+export async function generateMetadata() {
+  const page = await queryHomepage();
+
+  return {
+    openGraph: {
+      title: page.data.meta_title,
+      // description: prismic.asText(page.data.meta_description),
+      // in the tutorial, the description field is just a plain text field, so prismic.asText() throws an error. this should work if it's a rich text field.
+      description: page.data.meta_description,
+      images: prismic.asImageSrc(page.data.meta_image),
+    },
+  };
+}
+
+export default async function Home() {
+  const page = await queryHomepage();
+
   return (
     <main>
       <div>Hello world</div>
