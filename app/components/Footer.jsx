0bdf40932dd1e430e486626fdfcaef9a650815b7
jessicagallagher <jessicalyngallagher@me.com>
2024-02-29 12:08:26 -0500

    fixed nav by adding nav content component and removing code from layout

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,11 +1,34 @@
-// import { createClient } from '@/prismicio';
-// import { PrismicNextLink, PrismicNextImage } from '@prismicio/next';
+import { useState, useEffect } from 'react';
+import { createClient } from '@/prismicio';
+import { PrismicNextLink, PrismicNextImage } from '@prismicio/next';
 
-// export default async function Footer() {
-//   const client = createClient();
-//   const footer = await client.getSingle('footer')
+export default async function Footer() {
+  const [date, setDate] = useState();
+  const client = createClient();
+  const footer = await client.getSingle('footer')
 
-//   return (
-//     <div>Footer</div>
-//   )
-// }
+  const getYear = () => setDate(new Date().getFullYear());
+
+  useEffect(() => {
+    getYear();
+  }, []);
+
+  return (
+    <footer className='font-semilbold'>
+      <div className='max-w-full mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px8'>
+        <nav className='-mx-5 -my-2 flex flex-wrap justify-center' aria-label='Footer'>
+          {footer.data.menu_items.map((item) => {
+            return (
+              <li key={JSON.stringify(item)}>
+                <PrismicNextLink field={item.link}>{item.label}</PrismicNextLink>
+                
+    </li>
+  )
+})}
+        </nav>
+
+      </div>
+
+    </footer>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,10 @@
 'use client'
+import { useState } from 'react';
 import { PrismicPreview, PrismicNextLink, PrismicNextImage } from '@prismicio/next';
 import { repositoryName, createClient } from '@/prismicio';
 import { usePathname, useRouter } from 'next/navigation';
 import './globals.css';
-// import NavBar from './components/NavBar';
+import NavBar from './components/NavBar';
 // import Footer from './components/Footer';
 
 export default function RootLayout({ children }) {
@@ -19,40 +20,91 @@ export default function RootLayout({ children }) {
   );
 }
 
-async function NavBar() {
-  const pathname = usePathname();
-  const router = useRouter();
-  const client = createClient();
-  const nav = await client.getSingle('navigation_menu');
+// async function NavBar() {
+//   const pathname = usePathname();
+//   const router = useRouter();
+//   const client = createClient();
+//   const nav = await client.getSingle('navigation_menu');
 
-  const inactive = 'hover:border-purpleDefault hover:border-b-2';
-  const active = 'border-lavender border-b-2';
+//   const inactive = 'hover:border-purpleDefault hover:border-b-2';
+//   const active = 'border-lavender border-b-2';
 
-  return (
-    <div className='flex justify-center items-center font-semibold max-w-full mx-auto py-2'>
-      <div className='container flex justify-between'>
-        <span className='text-2xl leading-6 font-logo flex flex-row items-center'>
-          {nav.data.company_name}
-          <PrismicNextImage
-            field={nav.data.company_logo}
-            className='h-20 w-20 ml-4'
-          />
-        </span>
-        <ul className='flex items-center text-2xl'>
-          {nav.data.menu_items.map((item) => {
-            return (
-              <li key={JSON.stringify(item)}>
-                <PrismicNextLink
-                  field={item.link}
-                  className={`no-underline ${(router.pathname = item.link ? active : inactive)}`}
-                >
-                  {item.label}
-                </PrismicNextLink>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className='flex justify-center items-center font-semibold max-w-full mx-auto py-2'>
+//       <div className='container flex justify-between'>
+//         <span className='text-2xl leading-6 font-logo flex flex-row items-center'>
+//           {nav.data.company_name}
+//           <PrismicNextImage
+//             field={nav.data.company_logo}
+//             className='h-20 w-20 ml-4'
+//           />
+//         </span>
+//         <ul className='flex items-center text-2xl'>
+//           {nav.data.menu_items.map((item) => {
+//             return (
+//               <li key={JSON.stringify(item)}>
+//                 <PrismicNextLink
+//                   field={item.link}
+//                   className={`no-underline ${(router.pathname = item.link ? active : inactive)}`}
+//                 >
+//                   {item.label}
+//                 </PrismicNextLink>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// async function Footer() {
+//   const [date, setDate] = useState();
+//   const client = createClient();
+//   const footer = await client.getSingle('footer');
+
+//   const getYear = () => setDate(new Date().getFullYear());
+
+//   getYear();
+
+//   return (
+//     <footer className='font-semilbold'>
+//       <div className='max-w-full mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px8'>
+//         <nav
+//           className='-mx-5 -my-2 flex flex-wrap justify-center'
+//           aria-label='Footer'
+//         >
+//           {footer.data.menu_items.map((item) => {
+//             return (
+//               <ul className='list-none'>
+//                 <li key={JSON.stringify(item)}>
+//                   <PrismicNextLink field={item.link} className='no-underline'>
+//                     {item.label}
+//                   </PrismicNextLink>
+//                 </li>
+//               </ul>
+//             );
+//           })}
+//         </nav>
+//         <div className='flex flex-row items-center justify-center my-6 text-center'>
+//           <span className='mr-2 flex flex-row items-center font-logo text-2xl leading-6'>
+//             {footer.data.company_name}
+//             <PrismicNextImage
+//               field={footer.data.company_logo}
+//               className='h-20 w-20 ml-4'
+//             />
+//           </span>
+//         </div>
+//         <div className='flex justify-center items-center'>
+//           <p>
+//             Brooklyn, NY |{' '}
+//             <a href='mailto:hello@techmeowt.com'>hello@techmeowt.com</a>
+//           </p>
+//         </div>
+//         <p className='text-center mt-2'>
+
+//         </p>
+//       </div>
+//     </footer>
+//   );
+// }
