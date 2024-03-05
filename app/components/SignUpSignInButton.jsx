700346710298059cc0f3d0ee02d349a825c36c34
jessicagallagher <jessicalyngallagher@me.com>
2024-03-05 12:31:10 -0500

    fixed css on all button components

--- app/components/SignUpSignInButton.jsx
+++ app/components/SignUpSignInButton.jsx
@@ -1,9 +1,8 @@
-'use client'
 import Link from 'next/link';
 
 export default function SignUpSignInButton({ buttonText, buttonLink }) {
   return (
-      <button className='shadow shadow-2xl text-xl font-raleway rounded-md px-3.5 py-2.5 text-sm font-semibold hover:outline hover:outline-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white text-purpleDefault hover:bg-purpleDefault hover:text-white border-purpleDefault border-2 hover:border-lavender'>
+      <button className='shadow shadow-2xl font-raleway rounded-md px-3 py-2 text-base font-semibold hover:outline hover:outline-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-white text-purpleDefault hover:bg-purpleDefault hover:text-white border-purpleDefault border-2 hover:border-lavender'>
         {buttonText}
         <Link href={`${buttonLink}`} />
       </button>
