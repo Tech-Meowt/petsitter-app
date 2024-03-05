062f42e31cefa1027d325742511e6101fb33fa65
jessicagallagher <jessicalyngallagher@me.com>
2024-03-05 17:21:43 -0500

    create frontend sign up page

--- app/components/SignUpSignInForm.jsx
+++ app/components/SignUpSignInForm.jsx
@@ -15,23 +15,53 @@ export default function SignUpSignInForm() {
               src={logo}
               loading='lazy'
               alt='cat logo'
-              className='h-10 w-auto'
+              className='h-20 w-auto'
             />
-            <h2 className='mt-8 text-2xl font-bold leading-9 tracking-tight'>
-              Sign in to your account
+            <h2 className='mt-8 font-bold leading-9 tracking-tight'>
+              Sign up for an account
             </h2>
             <p className='mt-2 text-base leading-6 font-semibold text-red-600'>
-              Don't have an account?{' '}
+              Already have an account?{' '}
               <Link href='/' className='no-underline hover:text-purpleDefault'>
-                Sign up here
+                Log in here
               </Link>
             </p>
           </div>
           <div className='mt-10'>
             <div>
               <form>
-                {/* email address */}
+                {/* first name */}
                 <div>
+                  <label htmlFor='firstName' className='block leading-6'>
+                    First name
+                  </label>
+                  <div className='mt-2'>
+                    <input
+                      id='firstName'
+                      name='firstName'
+                      type='text'
+                      required
+                      className='block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-purpleDefault sm:leading-6'
+                    />
+                  </div>
+                </div>
+                {/* last name */}
+                <div className='mt-2'>
+                  <label htmlFor='lastName' className='block leading-6'>
+                    Last name
+                  </label>
+                  <div className='mt-2'>
+                    <input
+                      id='lastName'
+                      name='lastName'
+                      type='text'
+                      required
+                      className='block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-purpleDefault sm:leading-6'
+                    />
+                  </div>
+                </div>
+                {/* email address */}
+                <div className='mt-2'>
                   <label htmlFor='email' className='block leading-6'>
                     Email address
                   </label>
@@ -75,9 +105,9 @@ export default function SignUpSignInForm() {
                   </div>
                 </div>
                 {/* submit button */}
-                <div className='mt-2 flex'>
+                <div className='mt-10 flex'>
                   <SignUpSignInButton
-                    buttonText='Sign in'
+                    buttonText='Submit'
                     buttonWidth='w-full'
                     type='submit'
                   />
@@ -85,9 +115,16 @@ export default function SignUpSignInForm() {
               </form>
             </div>
             {/* end of form */}
-            
           </div>
         </div>
+      </div>
+      <div className='relative hidden w-0 flex-1 lg:flex px-4 py-12 sm:px-6 lg:px-20 xl:px-24'>
+        <Image
+          src={img}
+          loading='lazy'
+          alt='cat wearing bandana'
+          className='rounded-3xl object-cover'
+        />
       </div>
     </div>
   );
